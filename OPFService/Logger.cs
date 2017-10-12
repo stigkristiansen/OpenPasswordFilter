7d108b3ee6f13109fea8c3bcc00cc1d2f6c4d1b8
Amar Kulo <amar@irget.se>
2017-10-12 10:38:36 +0200

    .gitignore fix for not including Logger.cs
    Logger.cs was ignored by .gitignore *.log so I have added exclude for any .cs file so it should work now

---
+++ OPFService/Logger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.IO;
+
+namespace OPFService
+{
+    class Logger
+    {
+
+        public void logException(Exception e) {
+
+            Boolean loggingEnabled = Convert.ToBoolean(ConfigurationManager.AppSettings["OPFLoggingEnabled"]);
+
+            if (loggingEnabled) {
+                writeErrorToLog(e);
+            }
+
+        }
+
+        private void writeErrorToLog(Exception errorException) {
+
+            String filePath = ConfigurationManager.AppSettings["OPFLogFilePath"];
+
+            try {
+                using (StreamWriter writer = new StreamWriter(filePath, true)) {
+                    writer.WriteLine(
+                        DateTime.Now.ToString() + ": " +
+                        errorException.Message + Environment.NewLine +
+                        "StackTrace :" + errorException.StackTrace +
+                        Environment.NewLine);
+                }
+
+            } catch (Exception e) {
+
+            }
+
+        }
+
+    }
+}
